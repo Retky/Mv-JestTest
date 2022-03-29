47e3f742ccf0d47280b376a3e5849e0da407d772
Retky <jose.arredondo.a@outlook.com>
2022-03-29 15:09:54 -0600

    Add 'calculator' jTest for each method

--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,4 @@
-const {stringLength, reverseString} = require('./functions');
+import {stringLength, reverseString, Calculator} from './functions';
 
 // Task 1
 describe('stringLength', () => {
@@ -17,5 +17,68 @@ describe('stringLength', () => {
 describe('reverseString', () => {
   test('someText = txeTemos', () => {
     expect(reverseString('someText')).toBe('txeTemos');
+  });
+});
+
+// Task 3
+const calc = new Calculator();
+
+describe('Sum', () => {
+  test('Is a Function', () => {
+    expect(typeof calc.add).toBe('function');
+  });
+  test('10+3 = 13', () => {
+    expect(calc.add(10,3)).toBe(13);
+  });
+  test('8+8 = 16', () => {
+    expect(calc.add(8,8)).toBe(16);
+  });
+  test('0+4 = 4', () => {
+    expect(calc.add(0,4)).toBe(4);
+  });
+});
+
+describe('Rest', () => {
+  test('Is a Function', () => {
+    expect(typeof calc.subtract).toBe('function');
+  });
+  test('10-3 = 7', () => {
+    expect(calc.subtract(10,3)).toBe(7);
+  });
+  test('8-8 = 0', () => {
+    expect(calc.subtract(8,8)).toBe(0);
+  });
+  test('7-4 = 3', () => {
+    expect(calc.subtract(7,4)).toBe(3);
+  });
+});
+
+describe('Divide', () => {
+  test('Is a Function', () => {
+    expect(typeof calc.divide).toBe('function');
+  });
+  test('10/4 = 2.5', () => {
+    expect(calc.divide(10,4)).toBe(2.5);
+  });
+  test('8/8 = 1', () => {
+    expect(calc.divide(8,8)).toBe(1);
+  });
+  test('20/4 = 5', () => {
+    expect(calc.divide(20,4)).toBe(5);
+  });
+});
+
+describe('Multiply', () => {
+  test('Is a Function', () => {
+    expect(typeof calc.multiply).toBe('function');
+  });
+  test('10*3 = 30', () => {
+    expect(calc.multiply(10,3)).toBe(30);
+  });
+  test('8*8 = 64', () => {
+    expect(calc.multiply(8,8)).toBe(64);
+  });
+  test('1*4 = 4', () => {
+    expect(calc.multiply(1,4)).toBe(4);
   });
 });
