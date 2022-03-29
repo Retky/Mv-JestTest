5b8051d4b6efe81835062d8083d7ff6020b6b523
Retky <jose.arredondo.a@outlook.com>
2022-03-29 13:17:27 -0600

    Add 'task1' jTest return error cases

--- functions.test.js
+++ functions.test.js
@@ -2,7 +2,13 @@ const {stringLength} = require('./functions');
 
 // Task 1
 describe('stringLength', () => {
-  test('second == 6', () => {
-    expect(stringLength('second')).toBe(6);
+  test('Text == 4', () => {
+    expect(stringLength('Text')).toBe(4);
+  });
+  test('Empty return error', () => {
+    expect(stringLength('')).toBe('error');
+  });
+  test('TextOverTen return error', () => {
+    expect(stringLength('textOverTen')).toBe('error');
   });
 });
