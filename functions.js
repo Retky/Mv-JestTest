ef0834b9831943e0dd9996c492d075c66e4daf99
Retky <jose.arredondo.a@outlook.com>
2022-03-29 13:16:34 -0600

    Modify 'task1' function expanded

--- functions.js
+++ functions.js
@@ -1,6 +1,11 @@
 // Task 1
 const stringLength = str => {
-  return str.length
+  const length = str.length;
+  if (length > 0 && length < 10) {
+    return length
+  } else {
+    return 'error'
+  }
 };
 
 module.exports = {stringLength};
